ad1a536b5e81024179839f120d5db3b4e854d361
Hebert Porto <hebertporto@gmail.com>
2017-02-07 22:45:39 -0200

    finalizando login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
   onEmailChange(text) {
@@ -16,6 +16,18 @@ class LoginForm extends Component {
   onButtonPress() {
     const { email, password } = this.props;
     this.props.loginUser({ email, password });
+  }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large" />;
+    }
+
+    return (
+       <Button onPress={this.onButtonPress.bind(this)}>
+         Login
+       </Button>
+    );
   }
 
    render() {
@@ -44,9 +56,7 @@ class LoginForm extends Component {
             {this.props.error}
           </Text>
           <CardSection>
-            <Button onPress={this.onButtonPress.bind(this)}>
-              Log In
-            </Button>
+            {this.renderButton()}
           </CardSection>
 
        </Card>
@@ -55,9 +65,9 @@ class LoginForm extends Component {
 }
 
 const mapStateToProps = ({ auth }) => {
-  const { email, password, error } = auth;
+  const { email, password, error, loading } = auth;
 
-  return { email, password, error };
+  return { email, password, error, loading };
 };
 
 const styles = {
